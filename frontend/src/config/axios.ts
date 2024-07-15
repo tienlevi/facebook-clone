95791bedc194a208fce8e072db4972eafd93dcd4
TiennLevii <nguyentrachtien2401@gmail.com>
2024-07-15 22:31:03 +0700

    update: url

--- frontend/src/config/axios.ts
+++ frontend/src/config/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const baseUrl = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: "https://facebook-clone-eomi.onrender.com/api",
 });
 
 export default baseUrl;
