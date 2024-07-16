1f834a21f5dba5b4387d1d48ff4401b220bb13c8
TiennLevii <nguyentrachtien2401@gmail.com>
2024-07-16 09:31:01 +0700

    allow all url

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,16 +8,10 @@ const app = express();
 Connect();
 
 app.use(express.json());
-app.use(
-  cors({
-    origin: ["http://localhost:3000", "http://facebooks-clonee.vercel.app"],
-    methods: ["GET", "POST", "DELETE", "PUT", "OPTIONS"],
-    credentials: true,
-  })
-);
+app.use(cors());
 app.use(morgan("dev"));
 
-app.use("/api", router);
+app.use("/api", cors(), router);
 
 app.listen(8080, () => {
   console.log("Server is running on port http://localhost:8080");
