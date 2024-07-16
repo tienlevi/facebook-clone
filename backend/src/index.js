ad6d7638b94bd1213149d693e7ab11fb1a566e2a
TiennLevii <nguyentrachtien2401@gmail.com>
2024-07-16 09:50:06 +0700

    fix cors again

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,21 +8,7 @@ const app = express();
 
 app.use(express.json());
 app.use(morgan("dev"));
-const allowedOrigins = [
-  "http://localhost:3000",
-  "https://facebooks-clonee.vercel.app",
-];
-const corsOptions = {
-  origin: (origin, callback) => {
-    if (allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  credentials: true,
-};
-app.use(cors(corsOptions));
+app.use(cors());
 
 Connect();
 app.use("/api", router);
