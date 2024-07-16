1ffb10510c97a675ec18b6fbb964c32af3109d88
TiennLevii <nguyentrachtien2401@gmail.com>
2024-07-16 23:59:11 +0700

    allow mixed

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -4,20 +4,6 @@ const nextConfig = {
   images: {
     domains: ["res.cloudinary.com"],
   },
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          {
-            key: "Content-Security-Policy",
-            value:
-              "default-src 'elf' https:; script-src 'elf' https: 'unsafe-inline' 'unsafe-eval'; style-src 'elf' https: 'unsafe-inline'; media-src 'elf' https: http://res.cloudinary.com;",
-          },
-        ],
-      },
-    ];
-  },
 };
 
 export default nextConfig;
