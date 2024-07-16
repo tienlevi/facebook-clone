8f7d8e77ff0664da8e52e068ab96382a2d4f9c91
TiennLevii <nguyentrachtien2401@gmail.com>
2024-07-16 09:39:49 +0700

    fix cors again

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,8 +7,15 @@ import router from "./router/router.js";
 const app = express();
 
 app.use(express.json());
-app.use(cors());
 app.use(morgan("dev"));
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "https://facebooks-clonee.vercel.app"],
+    methods: ["GET", "POST", "DELETE", "PUT", "OPTIONS"],
+    preflightContinue: false,
+    credentials: true,
+  })
+);
 
 Connect();
 app.use("/api", cors(), router);
