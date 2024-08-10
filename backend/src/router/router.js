4199986a26fb582eb0f3718c32a2d902ad6b05e4
TiennLevii <nguyentrachtien2401@gmail.com>
2024-08-10 17:47:21 +0700

    1

--- backend/src/router/router.js
+++ backend/src/router/router.js
@@ -13,7 +13,7 @@ const router = Router();
 
 // Auth
 router.post("/refresh-token", RefreshToken);
-router.get("/auth", AccessToken);
+router.get("/auth", AuthenticateToken, AccessToken);
 router.post("/register", Register);
 router.post("/login", Login, AuthenticateToken);
 // Posts
