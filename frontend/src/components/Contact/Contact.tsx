1a4c1e816708a0ec7c0e11c70a69d199d65841ea
TiennLevii <nguyentrachtien2401@gmail.com>
2024-11-16 23:58:48 +0700

    update file

--- frontend/src/components/Contact/Contact.tsx
+++ frontend/src/components/Contact/Contact.tsx
@@ -1,23 +1,9 @@
-import Image from "next/image";
-import { useImperativeHandle } from "react";
-
 function Contact() {
   return (
     <div className="sticky top-[60px] w-1/4 ml-5 h-screen">
       <h1 className="text-[19px] font-bold">Contact</h1>
       <div className="flex flex-col">
-        <div className="flex items-center my-3">
-          {/* <Image
-            src="https://res.cloudinary.com/dbjkk9wg0/image/upload/v1720775882/facebook-posts/xo41kgbhyxjfbdmfq4n6.png"
-            alt=""
-            width={35}
-            height={35}
-            className="rounded-full"
-          />
-          <div className="ml-2">
-            <p className="text-[15px] font-semibold">Nguyễn Trạch Tiến</p>
-          </div> */}
-        </div>
+        <div className="flex items-center my-3"></div>
       </div>
     </div>
   );
